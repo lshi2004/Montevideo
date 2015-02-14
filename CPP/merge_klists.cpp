fa4ed07fa656807ec81ec62947872d5ba5c75c54
lshi2004 <lshi2004@hotmail.com>
2015-02-14 08:30:15 -0800

    add priority queue implementation

--- CPP/merge_klists.cpp
+++ CPP/merge_klists.cpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <functional>
 #include <climits>
 
 using namespace std;
@@ -22,6 +24,36 @@ struct ListNode {
 
 class Solution {
 public:
+
+	struct cmp {
+		bool operator ()(const ListNode *a, const ListNode *b) {
+			return a->val > b->val;
+		}
+	};
+
+	ListNode *mergeKNewLists(vector<ListNode *> &lists) {
+		int n = lists.size();
+		if (n == 0) return nullptr;
+		ListNode node(0), *res = &node;
+
+		priority_queue<ListNode *, vector<ListNode *>, cmp> que;
+		for (int i = 0; i < n; i++)
+			if (lists[i])
+				que.push(lists[i]);
+
+		while (!que.empty()) {
+			ListNode *p = que.top();
+			que.pop();
+			res->next = p;
+			res = p;
+
+			if (p->next)
+				que.push(p->next);
+		}
+		return node.next;
+
+	}
+
 	ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
 		if (l1 == nullptr)
 			return l2;
