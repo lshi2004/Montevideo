83bb3b2838e42ce8c07aafecbb8d28476881c293
lshi2004 <lshi2004@hotmail.com>
2015-02-19 22:31:36 -0800

    add two number (LL)

---
+++ CPP/add_two_number.cpp
@@ -0,0 +1,90 @@
+//============================================================================
+// Name        : HC.cpp
+// Author      : HS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+class lengthSolution {
+public:
+
+	ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+		ListNode dummy(-1);
+		ListNode *p1, *p2, *previous;
+		int carry;
+
+		previous = &dummy;
+		carry = 0;
+		for (p1 = l1, p2 = l2; p1 != nullptr || p2 != nullptr;
+				previous = previous->next) {
+			int a1, a2, c;
+			a1 = p1 == nullptr ? 0 : p1->val;
+			a2 = p2 == nullptr ? 0 : p2->val;
+			c = a1 + a2 + carry;
+			carry = c / 10;
+			c = c % 10;
+			previous->next = new ListNode(c);
+			if (p1) p1 = p1->next;
+			if (p2) p2 = p2->next;
+
+		}
+
+		if (carry) previous->next = new ListNode(carry);
+		return dummy.next;
+	}
+
+};
+
+int main() {
+int *ptr = new int[10];
+//	int **ary = new int*[100];
+
+ptr[0] = 1;
+ptr[1] = 2;
+
+#if 0
+auto q = new multipleQueens;
+vector<vector<string>> result = q->solveQueens(4);
+
+for (auto i : result) {
+	for (auto j: i) {
+		cout << j << " \t";
+	}
+	cout << endl <<endl <<endl;
+}
+
+#endif
+
+auto pp = new lengthSolution;
+int k1 = 0;
+cout << "hello" << k1 << endl;
+
+//	for (int i = 1; i < 10; i++) {
+//			string s1 = pp->countAndSay(i);
+//			cout << s1 << endl;
+//	}
+
+cout << "!!!Hello World!!! " << ptr[0] << "   " << ptr[1] << endl; // prints !!!Hello World!!!
+return 0;
+}
