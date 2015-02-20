b233fbe63aaf554b40e78dbba1fd0b1df4cb2858
lshi2004 <lshi2004@hotmail.com>
2015-02-20 14:31:43 -0800

    rotate right

---
+++ CPP/rotate_right.cpp
@@ -0,0 +1,128 @@
+//============================================================================
+// Name        : HC.cpp
+// Author      : HS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+
+class lengthSolution {
+public:
+
+	ListNode *partition(ListNode *head, int x) {
+		ListNode left_dummy(-1);
+		ListNode right_dummy(-1);
+
+		auto left_cur = &left_dummy;
+		auto right_cur = &right_dummy;
+
+		for (ListNode *cur = head; cur; cur = cur->next) {
+			if (cur->val < x) {
+				left_cur->next = cur;
+				left_cur = cur;
+			} else {
+				right_cur->next = cur;
+				right_cur = cur;
+			}
+		}
+		left_cur->next = right_dummy.next;
+		right_cur->next = nullptr;
+
+		return left_dummy.next;
+
+	}
+
+    int majorityElement(vector<int> &num) {
+        int times = 1;
+        int last_num = num[0];
+
+        for (int i = 1; i < num.size(); ++i) {
+            if (num[i] == last_num) {
+                times++;
+            } else {
+                if (times > 0) times--;
+                else {
+                    times = 1;
+                    last_num = num[i];
+                }
+            }
+        }
+        return last_num;
+    }
+
+    ListNode *rotateRight(ListNode *head, int k) {
+    	if (head == nullptr) return head;
+
+    	int len = 1;
+    	ListNode *p = head;
+    	while (p->next) {
+    		len++;
+    		p = p->next;
+    	}
+
+    	k = len - k % len;
+    	p->next = head;
+    	for (int step = 0; step < k; step++) {
+    		p = p->next;
+    	}
+    	head = p->next;
+    	p->next = nullptr;
+    	return head;
+
+    }
+
+};
+
+int main() {
+	int *ptr = new int[10];
+//	int **ary = new int*[100];
+
+	ptr[0] = 1;
+	ptr[1] = 2;
+
+#if 0
+	auto q = new multipleQueens;
+	vector<vector<string>> result = q->solveQueens(4);
+
+	for (auto i : result) {
+		for (auto j: i) {
+			cout << j << " \t";
+		}
+		cout << endl <<endl <<endl;
+	}
+
+#endif
+
+//auto pp = new lengthSolution;
+	int k1 = 0;
+	cout << "hello" << k1 << endl;
+
+//	for (int i = 1; i < 10; i++) {
+//			string s1 = pp->countAndSay(i);
+//			cout << s1 << endl;
+//	}
+
+	cout << "!!!Hello World!!! " << ptr[0] << "   " << ptr[1] << endl; // prints !!!Hello World!!!
+	return 0;
+}
