453bc113e6c1d3dd2704fbd5a53e561b47b07ae1
lshi2004 <lshi2004@hotmail.com>
2015-02-20 23:08:14 -0800

    LRU cache

---
+++ CPP/lru_cache.cpp
@@ -0,0 +1,129 @@
+//============================================================================
+// Name        : HC.cpp
+// Author      : HS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <list>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) :
+			label(x), next(NULL), random(NULL) {
+	}
+};
+
+class lengthSolution {
+private:
+};
+
+class LRUCache {
+private:
+	struct CacheNode {
+		int key;
+		int value;
+		CacheNode(int k, int v) :
+				key(k), value(v) {
+		}
+	};
+
+	list<CacheNode> cacheList;
+	unordered_map<int, list<CacheNode>::iterator> cacheMap;
+	int capacity;
+
+public:
+	LRUCache(int capacity) {
+		this->capacity = capacity;
+	}
+
+	int get(int key) {
+		if (cacheMap.find(key) == cacheMap.end())
+			return -1;
+		cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+		cacheMap[key] = cacheList.begin();
+		return cacheMap[key]->value;
+	}
+
+	void set(int key, int value) {
+		unordered_map<int, list<CacheNode>::iterator>::const_iterator it;
+		it = cacheMap.find(key);
+
+		if (it == cacheMap.end()) {
+			if (cacheList.size() == capacity) {
+				cacheMap.erase(cacheList.back().key);
+				cacheList.pop_back();
+			}
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+		} else {
+			cacheList.erase(cacheMap[key]);
+			//	cacheList.erase(it->second);
+			cacheMap.erase(key);
+
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+
+		}
+	}
+
+};
+
+int main() {
+	int *ptr = new int[10];
+//	int **ary = new int*[100];
+
+	ptr[0] = 1;
+	ptr[1] = 2;
+
+#if 0
+	auto q = new multipleQueens;
+	vector<vector<string>> result = q->solveQueens(4);
+
+	for (auto i : result) {
+		for (auto j: i) {
+			cout << j << " \t";
+		}
+		cout << endl <<endl <<endl;
+	}
+
+#endif
+
+	auto pp = new lengthSolution;
+	ListNode np(1);
+//	ListNode *pn = pp->removeNthFromEnd(&np, 1);
+
+	int k1 = 0;
+	cout << "hello" << k1 << endl;
+
+//	for (int i = 1; i < 10; i++) {
+//			string s1 = pp->countAndSay(i);
+//			cout << s1 << endl;
+//	}
+
+	cout << "!!!Hello World!!! " << ptr[0] << "   " << ptr[1] << endl; // prints !!!Hello World!!!
+	return 0;
+}
