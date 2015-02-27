2f9f5b020d1a33ba24e2c061cd6bf8db2b8c7e73
lshi2004 <lshi2004@hotmail.com>
2015-02-26 21:18:53 -0800

    sort list

---
+++ CPP/sort_list.cpp
@@ -0,0 +1,212 @@
+//============================================================================
+// Name        : HC.cpp
+// Author      : HS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <list>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) :
+			label(x), next(NULL), random(NULL) {
+	}
+};
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+struct TreeLinkNode {
+	int val;
+	TreeLinkNode *left, *right, *next;
+	TreeLinkNode(int x) :
+			val(x), left(NULL), right(NULL), next(NULL) {
+	}
+};
+
+class lengthSolution {
+private:
+};
+
+class LRUCache {
+private:
+	struct CacheNode {
+		int key;
+		int value;
+		CacheNode(int k, int v) :
+				key(k), value(v) {
+		}
+	};
+
+	list<CacheNode> cacheList;
+	unordered_map<int, list<CacheNode>::iterator> cacheMap;
+	int capacity;
+
+public:
+	LRUCache(int capacity) {
+		this->capacity = capacity;
+	}
+
+	int get(int key) {
+		if (cacheMap.find(key) == cacheMap.end())
+			return -1;
+		cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+		cacheMap[key] = cacheList.begin();
+		return cacheMap[key]->value;
+	}
+
+	void set(int key, int value) {
+		//unordered_map<int, list<CacheNode>::iterator>::const_iterator it;
+		auto it = cacheMap.find(key);
+
+		if (it == cacheMap.end()) {
+			if (cacheList.size() == capacity) {
+				cacheMap.erase(cacheList.back().key);
+				cacheList.pop_back();
+			}
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+		} else {
+			cacheList.erase(cacheMap[key]);
+			//	cacheList.erase(it->second);
+			cacheMap.erase(key);
+
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+
+		}
+	}
+
+public:
+
+	ListNode *sortList(ListNode *head) {
+		if (!head || !head->next)
+			return head;
+
+		ListNode *fast = head, *slow = head;
+		while (fast->next && fast->next->next) {
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+
+		fast = slow;
+		slow = slow->next;
+		fast->next = nullptr;
+
+		ListNode *h1 = sortList(head);
+		ListNode *h2 = sortList(slow);
+
+		return mergeTwoLists(h1, h2);
+	}
+
+	ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+		if (!l1)
+			return l2;
+		if (!l2)
+			return l1;
+
+		ListNode dummy(INT_MIN), *p = &dummy;
+		ListNode *p1, *p2;
+
+		for (p1 = l1, p2 = l2; p1 && p2; p = p->next) {
+			if (p1->val <= p2->val) {
+				p->next = p1;
+				p1 = p1->next;
+			}
+			else {
+				p->next = p2;
+				p2 = p2->next;
+			}
+
+		}
+
+		if (!p1)
+			p->next = p2;
+		else
+			p->next = p1;
+
+		return dummy.next;
+
+	}
+};
+
+void printPreOrder(TreeNode *root) {
+	if (!root)
+		return;
+	cout << root->val << "\t";
+	if (root->left)
+		printPreOrder(root->left);
+	if (root->right)
+		printPreOrder(root->right);
+}
+
+void printInOrder(TreeNode *root) {
+	if (!root)
+		return;
+	if (root->left)
+		printPreOrder(root->left);
+	cout << root->val << "\t";
+	if (root->right)
+		printPreOrder(root->right);
+}
+
+int main() {
+	int *ptr = new int[10];
+//	int **ary = new int*[100];
+
+	ptr[0] = 1;
+	ptr[1] = 2;
+
+#if 0
+	auto q = new multipleQueens;
+	vector<vector<string>> result = q->solveQueens(4);
+
+	for (auto i : result) {
+		for (auto j: i) {
+			cout << j << " \t";
+		}
+		cout << endl <<endl <<endl;
+	}
+
+#endif
+
+//	auto pp = new LRUCache(100);
+
+	string ts1 = "aa";
+	cout << "size of aa is " << ts1.size() << endl;
+
+	cout << "!!!Hello World!!! " << ptr[0] << "   " << ptr[1] << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
