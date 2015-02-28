5f3d64b0a5ab5501c2555c876ac69e58f1dd5fe1
lshi2004 <lshi2004@hotmail.com>
2015-02-27 22:10:39 -0800

    subset two

---
+++ CPP/subset_two.cpp
@@ -0,0 +1,260 @@
+//============================================================================
+// Name        : HC.cpp
+// Author      : HS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <list>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) :
+			label(x), next(NULL), random(NULL) {
+	}
+};
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+struct TreeLinkNode {
+	int val;
+	TreeLinkNode *left, *right, *next;
+	TreeLinkNode(int x) :
+			val(x), left(NULL), right(NULL), next(NULL) {
+	}
+};
+
+class lengthSolution {
+private:
+};
+
+class LRUCache {
+private:
+	struct CacheNode {
+		int key;
+		int value;
+		CacheNode(int k, int v) :
+				key(k), value(v) {
+		}
+	};
+
+	list<CacheNode> cacheList;
+	unordered_map<int, list<CacheNode>::iterator> cacheMap;
+	int capacity;
+
+public:
+	LRUCache(int capacity) {
+		this->capacity = capacity;
+	}
+
+	int get(int key) {
+		if (cacheMap.find(key) == cacheMap.end())
+			return -1;
+		cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+		cacheMap[key] = cacheList.begin();
+		return cacheMap[key]->value;
+	}
+
+	void set(int key, int value) {
+		//unordered_map<int, list<CacheNode>::iterator>::const_iterator it;
+		auto it = cacheMap.find(key);
+
+		if (it == cacheMap.end()) {
+			if (cacheList.size() == capacity) {
+				cacheMap.erase(cacheList.back().key);
+				cacheList.pop_back();
+			}
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+		} else {
+			cacheList.erase(cacheMap[key]);
+			//	cacheList.erase(it->second);
+			cacheMap.erase(key);
+
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+
+		}
+	}
+
+public:
+#if 0
+	vector<vector<int>> subsets(vector<int> &S) {
+		sort(S.begin(), S.end());
+		vector<vector<int>> result;
+		vector<int> path;
+		subsets(S, path, 0, result);
+		return result;
+	}
+
+	void subsets(vector<int> &S, vector<int> &path, int step,
+				vector<vector<int>> &result) {
+
+		if (step == S.size()) {
+			result.push_back(path);
+			cout << "result push" << endl;
+			return;
+		}
+		subsets(S, path, step + 1, result);
+		path.push_back(S[step]);
+		cout << "pushed  " << S[step] << endl;
+		subsets(S, path, step + 1, result);
+		cout << "should pop but not"<< endl;
+		path.pop_back();
+	}
+#endif
+	vector<vector<int>> subsets(vector<int> &S) {
+		sort(S.begin(), S.end());
+		vector<vector<int>> result;
+		vector<bool> selected(S.size(), false);
+
+		subsets(S, selected, 0, result);
+		return result;
+	}
+
+	void subsets(const vector<int> &S, vector<bool> &selected, int step,
+				 vector<vector<int>> &result) {
+		if (step == S.size()) {
+			vector<int> subset;
+			for (int i = 0; i < S.size(); i++) {
+				if (selected[i]) subset.push_back(S[i]);
+			}
+			result.push_back(subset);
+			return;
+		}
+		selected[step] = false;
+		subsets(S, selected, step + 1, result);
+		selected[step] = true;
+		subsets(S, selected, step + 1, result);
+	}
+
+	vector<vector<int> > subsets_iterative(vector<int> &S) {
+		sort(S.begin(), S.end());
+		vector<vector<int>> result(1);
+
+		for (auto elem: S) {
+			result.reserve(result.size() * 2);
+			auto half = result.begin() + result.size();
+			copy(result.begin(), half, back_inserter(result));
+			for_each(half, result.end(), [&elem](decltype(result[0]) &e) {
+				e.push_back(elem);
+			});
+		}
+		return result;
+	}
+
+	vector<vector<int>> subsetsWithDup(vector<int> &S) {
+
+		//set<vector<int>> result2;
+		std::set<vector<int>> result;
+		sort(S.begin(), S.end());
+
+		const size_t n = S.size();
+		vector<int> v;
+
+		for (size_t i = 0; i < 1U << n; ++i) {
+			for (size_t j = 0; j < n; ++j) {
+				if (i & 1 << j)
+					v.push_back(S[j]);
+			}
+			result.insert(v);
+			v.clear();
+		}
+
+		vector<vector<int>> real_result;
+		copy(result.begin(), result.end(), back_inserter(real_result));
+		return real_result;
+
+	}
+};
+
+void printPreOrder(TreeNode *root) {
+	if (!root)
+		return;
+	cout << root->val << "\t";
+	if (root->left)
+		printPreOrder(root->left);
+	if (root->right)
+		printPreOrder(root->right);
+}
+
+void printInOrder(TreeNode *root) {
+	if (!root)
+		return;
+	if (root->left)
+		printPreOrder(root->left);
+	cout << root->val << "\t";
+	if (root->right)
+		printPreOrder(root->right);
+}
+
+int main() {
+	int *ptr = new int[10];
+//	int **ary = new int*[100];
+
+	ptr[0] = 1;
+	ptr[1] = 2;
+
+#if 0
+	auto q = new multipleQueens;
+	vector<vector<string>> result = q->solveQueens(4);
+
+	for (auto i : result) {
+		for (auto j: i) {
+			cout << j << " \t";
+		}
+		cout << endl <<endl <<endl;
+	}
+
+#endif
+
+	auto pp = new LRUCache(100);
+	vector<int>	S{1, 2, 3};
+	auto result = pp->subsets(S);
+
+	for (auto i: result) {
+		for (auto j: i)
+			cout << j << "\t";
+		cout << endl << "one more result " << endl << endl;
+	}
+
+	cout << endl <<endl <<endl;
+	string ts1 = "aa";
+	cout << "size of aa is " << ts1.size() << endl;
+
+	cout << "!!!Hello World!!! " << ptr[0] << "   " << ptr[1] << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
