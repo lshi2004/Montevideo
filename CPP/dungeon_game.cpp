de122529c04cc9e0f458755923267c80a4e8cc2d
lshi2004 <lshi2004@hotmail.com>
2015-03-02 12:40:59 -0800

    dungeon game

---
+++ CPP/dungeon_game.cpp
@@ -0,0 +1,215 @@
+//============================================================================
+// Name        : HC.cpp
+// Author      : HS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <list>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) :
+			label(x), next(NULL), random(NULL) {
+	}
+};
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+struct TreeLinkNode {
+	int val;
+	TreeLinkNode *left, *right, *next;
+	TreeLinkNode(int x) :
+			val(x), left(NULL), right(NULL), next(NULL) {
+	}
+};
+
+class lengthSolution {
+private:
+};
+
+
+
+bool int_sort_function_old (int i, int j) {
+	string s1 = to_string(i);
+	string s2 = to_string(j);
+	int k1 , k2;
+
+	for (k1 = 0, k2 = 0; k1 < s1.size() && k2 < s2.size(); k1++, k2++) {
+		if (s1[k1] != s2[k2]) {
+			return s1[k1] < s2[k2];
+		}
+	}
+
+	return (k1 >= s1.size() );
+}
+
+bool int_sort_function(int i, int j) {
+	string s1 = to_string(i);
+	string s2 = to_string(j);
+	return s1 + s2 < s2 + s1;
+}
+
+class LRUCache {
+private:
+	struct CacheNode {
+		int key;
+		int value;
+		CacheNode(int k, int v) :
+				key(k), value(v) {
+		}
+	};
+
+	list<CacheNode> cacheList;
+	unordered_map<int, list<CacheNode>::iterator> cacheMap;
+	int capacity;
+
+public:
+	LRUCache(int capacity) {
+		this->capacity = capacity;
+	}
+
+	int get(int key) {
+		if (cacheMap.find(key) == cacheMap.end())
+			return -1;
+		cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+		cacheMap[key] = cacheList.begin();
+		return cacheMap[key]->value;
+	}
+
+	void set(int key, int value) {
+		//unordered_map<int, list<CacheNode>::iterator>::const_iterator it;
+		auto it = cacheMap.find(key);
+
+		if (it == cacheMap.end()) {
+			if (cacheList.size() == capacity) {
+				cacheMap.erase(cacheList.back().key);
+				cacheList.pop_back();
+			}
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+		} else {
+			cacheList.erase(cacheMap[key]);
+			//	cacheList.erase(it->second);
+			cacheMap.erase(key);
+
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+
+		}
+	}
+
+public:
+
+	int calculateMinimumHP(vector<vector<int> > &dungeon) {
+		if (dungeon.size() == 0) return 0;
+
+		int height = dungeon.size();
+		int row = dungeon[0].size();
+
+		vector<vector<int>> f(height + 1, vector<int>(row + 1, INT_MAX));
+		f[height][row] = f[height - 1][row] = f[height][row - 1] = 1;
+
+		for (int i = height - 1; i >= 0; i--)
+				for (int j = row -1; j >= 0; j--)
+					f[i][j] = max(1, min(f[i+1][j], f[i][j+1]) - dungeon[i][j]);
+
+		return f[0][0];
+	}
+
+};
+
+
+void printPreOrder(TreeNode *root) {
+	if (!root)
+		return;
+	cout << root->val << "\t";
+	if (root->left)
+		printPreOrder(root->left);
+	if (root->right)
+		printPreOrder(root->right);
+}
+
+void printInOrder(TreeNode *root) {
+	if (!root)
+		return;
+	if (root->left)
+		printPreOrder(root->left);
+	cout << root->val << "\t";
+	if (root->right)
+		printPreOrder(root->right);
+}
+
+int main() {
+	int *ptr = new int[10];
+//	int **ary = new int*[100];
+
+	ptr[0] = 1;
+	ptr[1] = 2;
+
+#if 0
+	auto q = new multipleQueens;
+	vector<vector<string>> result = q->solveQueens(4);
+
+	for (auto i : result) {
+		for (auto j: i) {
+			cout << j << " \t";
+		}
+		cout << endl <<endl <<endl;
+	}
+
+#endif
+
+//	auto pp = new LRUCache(100);
+
+
+#if 0
+	auto result = pp->partitionN("a");
+	for (auto i: result) {
+		for (auto j: i)
+		cout << j << "\t";
+		cout << endl << "one more result " << endl << endl;
+	}
+#endif
+
+	cout << endl << endl << endl;
+	string ts1 = "aa";
+	cout << "size of aa is " << ts1.size() << endl;
+
+	cout << "!!!Hello World!!! " << ptr[0] << "   " << ptr[1] << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
