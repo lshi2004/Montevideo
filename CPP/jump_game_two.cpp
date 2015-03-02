9394c8095030090cec2c42867360189876472925
lshi2004 <lshi2004@hotmail.com>
2015-03-01 21:35:59 -0800

    jump game two working

--- CPP/jump_game_two.cpp
+++ CPP/jump_game_two.cpp
@@ -112,26 +112,28 @@ public:
 
 public:
 	int jump_two(int A[], int n) {
-	        int step = 0; // 最小步数
-	        int left = 0;
+		int step = 0;
+		int left = 0;
 
-	        int right = 0; // [left, right] 是当前能覆盖的区间
-	        if (n == 1) return 0;
-	        while (left <= right) { // 尝试从每一层跳最远
-	                ++step;
-	              const int old_right = right;
-	              for (int i = left; i <= old_right; ++i) {
-	                  int new_right = i + A[i];
-	                  if (new_right >= n - 1) return step;
-	                  if (new_right > right) right = new_right;
-	              }
-	              left = old_right + 1;
-	          }
-	        return 0;
+		int right = 0;
+		if (n == 1)
+			return 0;
 
-	    }
+		while (left <= right) {
+			++step;
+			const int old_right = right;
+			for (int i = left; i <= old_right; ++i) {
+				int new_right = i + A[i];
+				if (new_right >= n - 1)
+					return step; // reach the end
+				if (new_right > right)
+					right = new_right;
+			}
+			left = old_right + 1;
+		}
+		return 0;
 
-
+	}
 
 };
 
