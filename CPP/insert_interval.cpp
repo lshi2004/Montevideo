e5b903e794d99a9343b7d9b4c8bdf176fd2c6a4d
lshi2004 <lshi2004@hotmail.com>
2015-03-05 23:13:27 -0800

    insert interval

---
+++ CPP/insert_interval.cpp
@@ -0,0 +1,228 @@
+//============================================================================
+// Name        : HC.cpp
+// Author      : HS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <list>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) :
+			label(x), next(NULL), random(NULL) {
+	}
+};
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+struct TreeLinkNode {
+	int val;
+	TreeLinkNode *left, *right, *next;
+	TreeLinkNode(int x) :
+			val(x), left(NULL), right(NULL), next(NULL) {
+	}
+};
+
+struct Interval {
+	int start;
+	int end;
+	Interval() :
+			start(0), end(0) {
+	}
+	Interval(int s, int e) :
+			start(s), end(e) {
+	}
+};
+
+struct UndirectedGraphNode {
+	int label;
+	vector<UndirectedGraphNode *> neighbors;
+	UndirectedGraphNode(int x) :
+			label(x) {
+	}
+	;
+};
+
+bool int_sort_function(int i, int j) {
+	string s1 = to_string(i);
+	string s2 = to_string(j);
+	return s1 + s2 < s2 + s1;
+}
+
+class LRUCache {
+private:
+	struct CacheNode {
+		int key;
+		int value;
+		CacheNode(int k, int v) :
+				key(k), value(v) {
+		}
+	};
+
+	list<CacheNode> cacheList;
+	unordered_map<int, list<CacheNode>::iterator> cacheMap;
+	int capacity;
+
+public:
+	LRUCache(int capacity) {
+		this->capacity = capacity;
+	}
+
+	int get(int key) {
+		if (cacheMap.find(key) == cacheMap.end())
+			return -1;
+		cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+		cacheMap[key] = cacheList.begin();
+		return cacheMap[key]->value;
+	}
+
+	void set(int key, int value) {
+		//unordered_map<int, list<CacheNode>::iterator>::const_iterator it;
+		auto it = cacheMap.find(key);
+
+		if (it == cacheMap.end()) {
+			if (cacheList.size() == capacity) {
+				cacheMap.erase(cacheList.back().key);
+				cacheList.pop_back();
+			}
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+		} else {
+			cacheList.erase(cacheMap[key]);
+			//	cacheList.erase(it->second);
+			cacheMap.erase(key);
+
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+
+		}
+	}
+
+public:
+	vector<Interval> insert1(vector<Interval> &intervals,
+			Interval newInterval) {
+		vector<Interval> result;
+		auto it = intervals.begin();
+		bool inserted = false;
+		while (it != intervals.end()) {
+			if (inserted) {
+				result.push_back(*it);
+			} else {
+				if (it->start > newInterval.end) {
+					result.push_back(newInterval);
+					result.push_back(*it);
+					inserted = true;
+				} else if (it->end < newInterval.start) {
+					result.push_back(*it);
+				} else {
+					newInterval.start = min(it->start, newInterval.start);
+					newInterval.end = max(it->end, newInterval.end);
+				}
+			}
+			++it;
+		}
+
+		if (inserted == false)
+			result.push_back(newInterval);
+		return result;
+	}
+
+	//
+	// this one will time exceeds...
+	// as erase will need to move the member up..
+	vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+		auto it = intervals.begin();
+
+		while (it != intervals.end()) {
+			if (it->start > newInterval.end) {
+				intervals.insert(it, newInterval);
+				return intervals;
+			} else if (it->end < newInterval.start) {
+				++it;
+			} else {
+				newInterval.start = min(it->start, newInterval.start);
+				newInterval.end = max(it->end, newInterval.end);
+				it = intervals.erase(it);
+			}
+		}
+
+		intervals.push_back(newInterval);
+		return intervals;
+	}
+
+};
+
+int main() {
+	int *ptr = new int[10];
+//	int **ary = new int*[100];
+
+	ptr[0] = 1;
+	ptr[1] = 2;
+
+#if 0
+	auto q = new multipleQueens;
+	vector<vector<string>> result = q->solveQueens(4);
+
+	for (auto i : result) {
+		for (auto j: i) {
+			cout << j << " \t";
+		}
+		cout << endl <<endl <<endl;
+	}
+
+#endif
+
+//	auto pp = new LRUCache(100);
+
+#if 0
+	auto result = pp->partitionN("a");
+	for (auto i: result) {
+		for (auto j: i)
+		cout << j << "\t";
+		cout << endl << "one more result " << endl << endl;
+	}
+#endif
+
+	string s1 = "1.0";
+	string s2 = "1.1";
+
+	string ts1 = "aa";
+
+	cout << "!!!Hello World!!! " << ptr[0] << "   " << ptr[1] << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
