0924dcd7cdeb07b3b30641b26794fe2b23c3dd1b
lshi2004 <lshi2004@hotmail.com>
2015-03-07 09:44:09 -0800

    find peak element

---
+++ CPP/find_peak.cpp
@@ -0,0 +1,223 @@
+//============================================================================
+// Name        : HC.cpp
+// Author      : HS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <list>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) :
+			label(x), next(NULL), random(NULL) {
+	}
+};
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+struct TreeLinkNode {
+	int val;
+	TreeLinkNode *left, *right, *next;
+	TreeLinkNode(int x) :
+			val(x), left(NULL), right(NULL), next(NULL) {
+	}
+};
+
+struct Interval {
+	int start;
+	int end;
+	Interval() :
+			start(0), end(0) {
+	}
+	Interval(int s, int e) :
+			start(s), end(e) {
+	}
+};
+
+struct UndirectedGraphNode {
+	int label;
+	vector<UndirectedGraphNode *> neighbors;
+	UndirectedGraphNode(int x) :
+			label(x) {
+	}
+	;
+};
+
+struct Point {
+	int x;
+	int y;
+	Point() :
+			x(0), y(0) {
+	}
+	Point(int a, int b) :
+			x(a), y(b) {
+	}
+};
+
+class LRUCache {
+private:
+	struct CacheNode {
+		int key;
+		int value;
+		CacheNode(int k, int v) :
+				key(k), value(v) {
+		}
+	};
+
+	list<CacheNode> cacheList;
+	unordered_map<int, list<CacheNode>::iterator> cacheMap;
+	int capacity;
+
+public:
+	LRUCache(int capacity) {
+		this->capacity = capacity;
+	}
+
+	int get(int key) {
+		if (cacheMap.find(key) == cacheMap.end())
+			return -1;
+		cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+		cacheMap[key] = cacheList.begin();
+		return cacheMap[key]->value;
+	}
+
+	void set(int key, int value) {
+		//unordered_map<int, list<CacheNode>::iterator>::const_iterator it;
+		auto it = cacheMap.find(key);
+
+		if (it == cacheMap.end()) {
+			if (cacheList.size() == capacity) {
+				cacheMap.erase(cacheList.back().key);
+				cacheList.pop_back();
+			}
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+		} else {
+			cacheList.erase(cacheMap[key]);
+			//	cacheList.erase(it->second);
+			cacheMap.erase(key);
+
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+
+		}
+	}
+
+public:
+	int findPeakElement(const vector<int> &num) {
+		int small = INT_MIN;
+		const int n = num.size();
+		for (int i = 0; i < n - 1; i++) {
+			if (i == n) {
+				if (num[i] > small)
+					return i;
+			} else {
+				if (num[i] > small && num[i] > num[i + 1])
+					return i;
+			}
+			small = num[i];
+		}
+
+		return -1;
+	}
+};
+
+int main() {
+	int *ptr = new int[10];
+//	int **ary = new int*[100];
+
+	ptr[0] = 1;
+	ptr[1] = 2;
+
+#if 0
+	auto q = new multipleQueens;
+	vector<vector<string>> result = q->solveQueens(4);
+
+	for (auto i : result) {
+		for (auto j: i) {
+			cout << j << " \t";
+		}
+		cout << endl <<endl <<endl;
+	}
+
+#endif
+
+	auto pp = new LRUCache(100);
+
+#if 0
+	auto result = pp->partitionN("a");
+	for (auto i: result) {
+		for (auto j: i)
+		cout << j << "\t";
+		cout << endl << "one more result " << endl << endl;
+	}
+#endif
+	vector<string> t1;
+	t1.push_back("a");
+	t1.push_back("b");
+	t1.push_back("c");
+	t1.push_back("d");
+	t1.push_back("e");
+
+	string s1 = "1.0";
+	string s2 = "1.1";
+
+	string ts1 = "aa";
+
+	cout << "!!!Hello World!!! " << ptr[0] << "   " << ptr[1] << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
+
+#if 0
+
+Suppose there are N elements and they range from A to B.
+
+Then the maximum gap will be no smaller than ceiling[(B - A) / (N - 1)]
+
+Let the length of a bucket to be len = ceiling[(B - A) / (N - 1)], then we will have at most num = (B - A) / len + 1 of bucket
+
+for any number K in the array, we can easily find out which bucket it belongs by calculating loc = (K - A) / len and therefore maintain the maximum and minimum elements in each bucket.
+
+Since the maximum difference between elements in the same buckets will be at most len - 1, so the final answer will not be taken from two elements in the same buckets.
+
+For each non-empty buckets p, find the next non-empty buckets q, then q.min - p.max could be the potential answer to the question. Return the maximum of all those values.
+
+Analysis written by @porker2008.
+
+#endif
+
