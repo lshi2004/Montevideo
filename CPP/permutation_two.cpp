7fd505b3d6b3c7705dcba55cb835a3f9a814c22f
lshi2004 <lshi2004@hotmail.com>
2015-03-23 16:16:20 -0700

    permutation two

---
+++ CPP/permutation_two.cpp
@@ -0,0 +1,221 @@
+//============================================================================
+// Name        : HC.cpp
+// Author      : HS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <list>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) :
+			label(x), next(NULL), random(NULL) {
+	}
+};
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+struct TreeLinkNode {
+	int val;
+	TreeLinkNode *left, *right, *next;
+	TreeLinkNode(int x) :
+			val(x), left(NULL), right(NULL), next(NULL) {
+	}
+};
+
+struct Interval {
+	int start;
+	int end;
+	Interval() :
+			start(0), end(0) {
+	}
+	Interval(int s, int e) :
+			start(s), end(e) {
+	}
+};
+
+struct UndirectedGraphNode {
+	int label;
+	vector<UndirectedGraphNode *> neighbors;
+	UndirectedGraphNode(int x) :
+			label(x) {
+	}
+	;
+};
+
+struct Point {
+	int x;
+	int y;
+	Point() :
+			x(0), y(0) {
+	}
+	Point(int a, int b) :
+			x(a), y(b) {
+	}
+};
+
+class LRUCache {
+private:
+	struct CacheNode {
+		int key;
+		int value;
+		CacheNode(int k, int v) :
+				key(k), value(v) {
+		}
+	};
+
+	list<CacheNode> cacheList;
+	unordered_map<int, list<CacheNode>::iterator> cacheMap;
+	int capacity;
+
+public:
+	LRUCache(int capacity) {
+		this->capacity = capacity;
+	}
+
+	int get(int key) {
+		if (cacheMap.find(key) == cacheMap.end())
+			return -1;
+		cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+		cacheMap[key] = cacheList.begin();
+		return cacheMap[key]->value;
+	}
+
+	void set(int key, int value) {
+		//unordered_map<int, list<CacheNode>::iterator>::const_iterator it;
+		auto it = cacheMap.find(key);
+
+		if (it == cacheMap.end()) {
+			if (cacheList.size() == capacity) {
+				cacheMap.erase(cacheList.back().key);
+				cacheList.pop_back();
+			}
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+		} else {
+			cacheList.erase(cacheMap[key]);
+			//	cacheList.erase(it->second);
+			cacheMap.erase(key);
+
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+
+		}
+	}
+
+public:
+
+	vector<vector<int> > permuteUnique(vector<int> &num) {
+
+		vector<vector<int>> result;
+		vector<int> path;
+		vector<int> visited(num.size() + 1, 0);
+
+		if (num.size() == 0) return result;
+
+		sort(num.begin(), num.end());
+		generate_permutation(num, 0, visited, path, result);
+
+		return result;
+	}
+
+
+	void generate_permutation(vector<int> &num, int step,
+			                  vector<int> &visited,
+							  vector<int> &path, vector<vector<int>> &result)
+	{
+		if (step == num.size()) {
+			result.push_back(path);
+			return;
+		}
+
+		for (int i = 0; i < num.size(); i++) {
+			if (visited[i] == 0) {
+				if (i > 0 && num[i] == num[i - 1] && visited[i - 1] == 0) {
+					continue;
+				}
+				visited[i] = 1;
+				path.push_back(num[i]);
+				generate_permutation(num, step + 1, visited,
+						             path, result);
+				path.pop_back();
+				visited[i] = 0;
+			}
+		}
+
+	}
+};
+
+
+
+
+int main() {
+	int *ptr = new int[10];
+//	int **ary = new int*[100];
+
+	ptr[0] = 1;
+	ptr[1] = 2;
+
+#if 0
+	auto q = new multipleQueens;
+	vector<vector<string>> result = q->solveQueens(4);
+
+	for (auto i : result) {
+		for (auto j: i) {
+			cout << j << " \t";
+		}
+		cout << endl <<endl <<endl;
+	}
+
+#endif
+
+	auto pp = new LRUCache(100);
+	vector<int> num{1, 2};
+	auto result  = pp->permuteUnique(num);
+
+	for (auto i : result) {
+		for (auto j: i) {
+			cout << j << "\t";
+		}
+		cout << endl << endl;
+
+	}
+
+	cout << "!!!Hello World!!! " << ptr[0] << "   " << ptr[1] << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
