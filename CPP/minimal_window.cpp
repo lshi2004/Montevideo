c53b4d6051aa7a9847d72f08dcd3c5007adb294b
lshi2004 <lshi2004@hotmail.com>
2015-03-28 15:41:02 -0700

    minimal window

---
+++ CPP/minimal_window.cpp
@@ -0,0 +1,220 @@
+//============================================================================
+// Name        : HC.cpp
+// Author      : HS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <list>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) :
+			label(x), next(NULL), random(NULL) {
+	}
+};
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+struct TreeLinkNode {
+	int val;
+	TreeLinkNode *left, *right, *next;
+	TreeLinkNode(int x) :
+			val(x), left(NULL), right(NULL), next(NULL) {
+	}
+};
+
+struct Interval {
+	int start;
+	int end;
+	Interval() :
+			start(0), end(0) {
+	}
+	Interval(int s, int e) :
+			start(s), end(e) {
+	}
+};
+
+struct UndirectedGraphNode {
+	int label;
+	vector<UndirectedGraphNode *> neighbors;
+	UndirectedGraphNode(int x) :
+			label(x) {
+	}
+	;
+};
+
+struct Point {
+	int x;
+	int y;
+	Point() :
+			x(0), y(0) {
+	}
+	Point(int a, int b) :
+			x(a), y(b) {
+	}
+};
+
+class LRUCache {
+private:
+	struct CacheNode {
+		int key;
+		int value;
+		CacheNode(int k, int v) :
+				key(k), value(v) {
+		}
+	};
+
+	list<CacheNode> cacheList;
+	unordered_map<int, list<CacheNode>::iterator> cacheMap;
+	int capacity;
+
+public:
+	LRUCache(int capacity) {
+		this->capacity = capacity;
+	}
+
+	int get(int key) {
+		if (cacheMap.find(key) == cacheMap.end())
+			return -1;
+		cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+		cacheMap[key] = cacheList.begin();
+		return cacheMap[key]->value;
+	}
+
+	void set(int key, int value) {
+		//unordered_map<int, list<CacheNode>::iterator>::const_iterator it;
+		auto it = cacheMap.find(key);
+
+		if (it == cacheMap.end()) {
+			if (cacheList.size() == capacity) {
+				cacheMap.erase(cacheList.back().key);
+				cacheList.pop_back();
+			}
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+		} else {
+			cacheList.erase(cacheMap[key]);
+			//	cacheList.erase(it->second);
+			cacheMap.erase(key);
+
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+
+		}
+	}
+
+public:
+
+
+	string minWindow(string S, string T) {
+
+		if (S.empty()) return "";
+		if (S.size() < T.size()) return "";
+
+		const int ASCII_MAX = 256;
+		int appeared_count[ASCII_MAX];
+		int expected_count[ASCII_MAX];
+
+		fill(appeared_count, appeared_count + ASCII_MAX, 0);
+		fill(expected_count, expected_count + ASCII_MAX, 0);
+
+		for (size_t i = 0; i < T.size(); i++)
+			expected_count[(unsigned int)T[i]]++;
+
+		int minWidth = INT_MAX, min_start = 0;
+		int wnd_start = 0;
+		int appeared = 0;
+
+		for (size_t wnd_end = 0; wnd_end < S.size(); wnd_end++) {
+			if (expected_count[(unsigned int)S[wnd_end]] > 0) {
+				appeared_count[(unsigned int)S[wnd_end]]++;
+				if (appeared_count[(unsigned int)S[wnd_end]] <=
+						expected_count[(unsigned int)S[wnd_end]]) {
+					appeared++;
+				}
+			}
+			if (appeared == T.size()) { //we got a T
+				while (appeared_count[(unsigned int)S[wnd_start]] >
+						expected_count[(unsigned int)S[wnd_start]]
+					 	 || expected_count[(unsigned int)S[wnd_start]] == 0) {
+					appeared_count[(unsigned int)S[wnd_start]]--;
+					wnd_start++;
+				}
+				if (minWidth > (wnd_end - wnd_start + 1)) {
+					minWidth = wnd_end - wnd_start + 1;
+					min_start = wnd_start;
+				}
+			}
+		}
+
+		if (minWidth == INT_MAX) return "";
+		else return S.substr(min_start, minWidth);
+
+	}
+
+
+
+
+
+};
+
+int main() {
+	int *ptr = new int[10];
+//	int **ary = new int*[100];
+
+	ptr[0] = 1;
+	ptr[1] = 2;
+
+#if 0
+	auto q = new multipleQueens;
+	vector<vector<string>> result = q->solveQueens(4);
+
+	for (auto i : result) {
+		for (auto j: i) {
+			cout << j << " \t";
+		}
+		cout << endl <<endl <<endl;
+	}
+
+#endif
+
+	auto pp = new LRUCache(100);
+
+	cout << "!!!Hello World!!! " << ptr[0] << "   " << ptr[1] << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
