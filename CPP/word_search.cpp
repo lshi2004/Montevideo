2b6a88a352fb9d3b2a3dc41189454de0485ff4c4
lshi2004 <lshi2004@hotmail.com>
2015-03-30 09:50:44 -0700

    word search

---
+++ CPP/word_search.cpp
@@ -0,0 +1,200 @@
+//============================================================================
+// Name        : HC.cpp
+// Author      : HS
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <numeric>
+#include <list>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) :
+			label(x), next(NULL), random(NULL) {
+	}
+};
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+struct TreeLinkNode {
+	int val;
+	TreeLinkNode *left, *right, *next;
+	TreeLinkNode(int x) :
+			val(x), left(NULL), right(NULL), next(NULL) {
+	}
+};
+
+struct Interval {
+	int start;
+	int end;
+	Interval() :
+			start(0), end(0) {
+	}
+	Interval(int s, int e) :
+			start(s), end(e) {
+	}
+};
+
+struct UndirectedGraphNode {
+	int label;
+	vector<UndirectedGraphNode *> neighbors;
+	UndirectedGraphNode(int x) :
+			label(x) {
+	}
+	;
+};
+
+struct Point {
+	int x;
+	int y;
+	Point() :
+			x(0), y(0) {
+	}
+	Point(int a, int b) :
+			x(a), y(b) {
+	}
+};
+
+class LRUCache {
+private:
+	struct CacheNode {
+		int key;
+		int value;
+		CacheNode(int k, int v) :
+				key(k), value(v) {
+		}
+	};
+
+	list<CacheNode> cacheList;
+	unordered_map<int, list<CacheNode>::iterator> cacheMap;
+	int capacity;
+
+public:
+	LRUCache(int capacity) {
+		this->capacity = capacity;
+	}
+
+	int get(int key) {
+		if (cacheMap.find(key) == cacheMap.end())
+			return -1;
+		cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+		cacheMap[key] = cacheList.begin();
+		return cacheMap[key]->value;
+	}
+
+	void set(int key, int value) {
+		//unordered_map<int, list<CacheNode>::iterator>::const_iterator it;
+		auto it = cacheMap.find(key);
+
+		if (it == cacheMap.end()) {
+			if (cacheList.size() == capacity) {
+				cacheMap.erase(cacheList.back().key);
+				cacheList.pop_back();
+			}
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+		} else {
+			cacheList.erase(cacheMap[key]);
+			//	cacheList.erase(it->second);
+			cacheMap.erase(key);
+
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+
+		}
+	}
+
+public:
+
+
+	 bool exist(vector<vector<char> > &board, string word) {
+	      	if(board.empty() || board[0].empty()) return false;
+
+			const int m = board.size();
+			const int n = board[0].size();
+
+			vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+			for (int i = 0; i < m; ++i)
+				for (int j = 0; j < n; ++j)
+					if (findWord(board, visited, i, j, word, 0))
+						return true;
+			return false;
+		}
+
+	bool findWord(vector<vector<char>> &board, vector<vector<bool>> &visited, int row, int col, string &word, int index) {
+	        if(index==word.size()) return true;
+	        if(row<0 || col<0 || row>=board.size() || col>=board[0].size() || visited[row][col] || board[row][col]!=word[index])
+	            return false;
+
+	        visited[row][col] = true;
+	        if(findWord(board, visited, row-1, col, word, index+1)) return true;
+	        if(findWord(board, visited, row+1, col, word, index+1)) return true;
+	        if(findWord(board, visited, row, col-1, word, index+1)) return true;
+	        if(findWord(board, visited, row, col+1, word, index+1)) return true;
+	        visited[row][col] = false;
+	        return false;
+	    }
+
+};
+
+int main() {
+	int *ptr = new int[10];
+//	int **ary = new int*[100];
+
+	ptr[0] = 1;
+	ptr[1] = 2;
+
+#if 0
+	auto q = new multipleQueens;
+	vector<vector<string>> result = q->solveQueens(4);
+
+	for (auto i : result) {
+		for (auto j: i) {
+			cout << j << " \t";
+		}
+		cout << endl <<endl <<endl;
+	}
+
+#endif
+
+	auto pp = new LRUCache(100);
+
+	cout << "!!!Hello World!!! " << ptr[0] << "   " << ptr[1] << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
